9659f041f1f7fd87fc94fbc2e96614aac6ff1ff0
Jennifer <69065554+Kamernepti@users.noreply.github.com>
2020-11-30 16:38:35 -0800

    Add files via upload

---
+++ Algorithms/algos.js
@@ -0,0 +1,53 @@
+//1.
+// var myNumber = 42;
+// var myName = "Jennifer";
+// var temp = 42;
+// myNumber = myName;
+// myName=temp;
+
+// console.log(myName);
+// console.log(myNumber);
+
+//2.
+// for(var i = -52; i <1067; i++){
+//     console.log(i);
+// }
+
+//3.
+// function beCheerful() {
+//     for (var i =0; i<98; i++) {
+//         console.log("Good Morning");
+//     }
+// }
+
+// beCheerful();
+
+//4.
+// for (var i = -300; i < 1; i += 3){
+//     if (i == -6 || i == -3){
+//     } else {
+//         console.log(i)
+//     }
+// }
+
+//5. 
+// var x= 2000;
+// while (x < 5281){
+//     console.log(x);
+//     x++;
+// }
+
+//6.
+
+
+//7.
+// var sum = 0
+// for (var i = 512; i < 4097; i ++) {
+//     if (i % 5 == 0){
+//         console.log(i);
+//         sum = sum + 1;
+//     }
+//     console.log(sum);
+// }
+
+//8. 
